9a33a9a03b4122d284600bb7ae025e675b285bc2
Luis-Caxi-Calani <eduardocalani@hotmail.com>
2024-08-14 23:34:07 -0500

    Initial commit

---
+++ src/components/Logo.tsx
@@ -0,0 +1,6 @@
+
+export default function Logo() {
+    return (
+        <img src="/logoHub.svg" alt="Logotipo FocusHub" className="w-24 md:w-32 lg:w-48 h-auto" />
+    )
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+import Router from './router'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+)
